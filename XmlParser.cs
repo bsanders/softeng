fda87bfcc52c83a6347535aa3174bbe554a7d342
Ryan Moe <rmoe88@gmail.com>
2013-02-15 17:13:37 -0800

    added comments to the XmlParser class

--- XmlParser.cs
+++ XmlParser.cs
@@ -1,4 +1,11 @@
-﻿using System;
+﻿/**
+ * By: Ryan Moe
+ * Edited Last: 
+ * 
+ * These are some backend tools to parse xml files.
+ **/
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Xml.Linq;
@@ -10,6 +17,13 @@ namespace SoftwareEng
     {
 
         //--------------------------------------------------------------
+        //By: Ryan Moe
+        //Edited Last: 
+        //
+        //Search an XElement's decendants.
+        //PARAM 1 = the parent XElement to search from.
+        //PARAM 2 = the children element's name to search the parent for.
+        //RETURNS = a list of XElements that you were looking for.
         public List<XElement> searchForElement(XElement parent, string lookingFor)
         {
             List<XElement> _list = new List<XElement>();
@@ -20,6 +34,13 @@ namespace SoftwareEng
             return _list;
         }//method
         //--------------------------------------------------------------
+        //By: Ryan Moe
+        //Edited Last: 
+        //
+        //Search an XDocuments's decendants.
+        //PARAM 1 = the parent XDocument to search from.
+        //PARAM 2 = the children element's name to search the parent for.
+        //RETURNS = a list of XElements that you were looking for.
         public List<XElement> searchForElement(XDocument parent, string lookingFor)
         {
             List<XElement> _list = new List<XElement>();
