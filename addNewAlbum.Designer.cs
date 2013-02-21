0a836226ef99c9bb65011dcccca946aed6bf2fab
nomoreclowns <sosar001@cougars.csusm.edu>
2013-02-21 11:46:29 -0800

    changed add new album window to meet modified specs

--- addNewAlbum.Designer.cs
+++ addNewAlbum.Designer.cs
@@ -28,14 +28,11 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(addNewAlbum));
             this.promptLabel = new System.Windows.Forms.Label();
             this.albumNameTextBox = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.radioButton1 = new System.Windows.Forms.RadioButton();
-            this.noRadioButton = new System.Windows.Forms.RadioButton();
             this.cancelButton = new System.Windows.Forms.Button();
-            this.NextButton = new System.Windows.Forms.Button();
-            this.backButton = new System.Windows.Forms.Button();
+            this.finishButton = new System.Windows.Forms.Button();
             this.selectFiles = new System.Windows.Forms.OpenFileDialog();
             this.SuspendLayout();
             // 
@@ -59,65 +56,26 @@
             this.albumNameTextBox.Size = new System.Drawing.Size(435, 20);
             this.albumNameTextBox.TabIndex = 1;
             // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(84, 99);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(259, 13);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "Would you like to add photos to this new album now?";
-            // 
-            // radioButton1
-            // 
-            this.radioButton1.AutoSize = true;
-            this.radioButton1.Location = new System.Drawing.Point(157, 141);
-            this.radioButton1.Name = "radioButton1";
-            this.radioButton1.Size = new System.Drawing.Size(43, 17);
-            this.radioButton1.TabIndex = 3;
-            this.radioButton1.TabStop = true;
-            this.radioButton1.Text = "Yes";
-            this.radioButton1.UseVisualStyleBackColor = true;
-            // 
-            // noRadioButton
-            // 
-            this.noRadioButton.AutoSize = true;
-            this.noRadioButton.Location = new System.Drawing.Point(217, 141);
-            this.noRadioButton.Name = "noRadioButton";
-            this.noRadioButton.Size = new System.Drawing.Size(39, 17);
-            this.noRadioButton.TabIndex = 4;
-            this.noRadioButton.TabStop = true;
-            this.noRadioButton.Text = "No";
-            this.noRadioButton.UseVisualStyleBackColor = true;
-            // 
             // cancelButton
             // 
             this.cancelButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-            this.cancelButton.Location = new System.Drawing.Point(13, 192);
+            this.cancelButton.Location = new System.Drawing.Point(13, 114);
             this.cancelButton.Name = "cancelButton";
             this.cancelButton.Size = new System.Drawing.Size(75, 23);
             this.cancelButton.TabIndex = 5;
             this.cancelButton.Text = "Cancel";
             this.cancelButton.UseVisualStyleBackColor = true;
             // 
-            // NextButton
+            // finishButton
             // 
-            this.NextButton.Location = new System.Drawing.Point(371, 191);
-            this.NextButton.Name = "NextButton";
-            this.NextButton.Size = new System.Drawing.Size(75, 23);
-            this.NextButton.TabIndex = 6;
-            this.NextButton.Text = "Next";
-            this.NextButton.UseVisualStyleBackColor = true;
-            this.NextButton.Click += new System.EventHandler(this.NextButton_Click);
-            // 
-            // backButton
-            // 
-            this.backButton.Location = new System.Drawing.Point(290, 191);
-            this.backButton.Name = "backButton";
-            this.backButton.Size = new System.Drawing.Size(75, 23);
-            this.backButton.TabIndex = 7;
-            this.backButton.Text = "Back";
-            this.backButton.UseVisualStyleBackColor = true;
+            this.finishButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.finishButton.Location = new System.Drawing.Point(372, 114);
+            this.finishButton.Name = "finishButton";
+            this.finishButton.Size = new System.Drawing.Size(75, 23);
+            this.finishButton.TabIndex = 6;
+            this.finishButton.Text = "Finish";
+            this.finishButton.UseVisualStyleBackColor = true;
+            this.finishButton.Click += new System.EventHandler(this.NextButton_Click);
             // 
             // selectFiles
             // 
@@ -128,17 +86,22 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(459, 227);
-            this.Controls.Add(this.backButton);
-            this.Controls.Add(this.NextButton);
+            this.ClientSize = new System.Drawing.Size(459, 149);
+            this.Controls.Add(this.finishButton);
             this.Controls.Add(this.cancelButton);
-            this.Controls.Add(this.noRadioButton);
-            this.Controls.Add(this.radioButton1);
-            this.Controls.Add(this.label1);
             this.Controls.Add(this.albumNameTextBox);
             this.Controls.Add(this.promptLabel);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(475, 187);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(475, 187);
             this.Name = "addNewAlbum";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.Text = "Add New Album";
+            this.TopMost = true;
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -148,12 +111,8 @@
 
         private System.Windows.Forms.Label promptLabel;
         private System.Windows.Forms.TextBox albumNameTextBox;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.RadioButton radioButton1;
-        private System.Windows.Forms.RadioButton noRadioButton;
         private System.Windows.Forms.Button cancelButton;
-        private System.Windows.Forms.Button NextButton;
-        private System.Windows.Forms.Button backButton;
+        private System.Windows.Forms.Button finishButton;
         private System.Windows.Forms.OpenFileDialog selectFiles;
     }
 }
