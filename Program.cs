2943da1f1598e97e09d228edb82f8b229432f849
nomoreclowns <sosar001@cougars.csusm.edu>
2013-03-04 00:39:04 -0800

    fixed odd bug that caused error popup saying .net version 4.5 needed to run program. Also only one instance of  program should be able to be run

--- Program.cs
+++ Program.cs
@@ -13,10 +13,29 @@ namespace SoftwareEng
         [STAThread]
         static void Main()
         {
+            
+            const bool ownThisMutex = true; 
+
+            //when specified as 'out' no need to initialize
+            bool mutexCreated;
+
+            System.Threading.Mutex mutex = new System.Threading.Mutex(ownThisMutex, "PhotoBomberMutex", out mutexCreated);
+
+            if (mutexCreated == false)
+            {
+                return;
+            }
+            
+
+
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new mainGUI());//main gui
             //Application.Run(new Form1());//backend testing gui
+
+            //keeps hold of mutex until program is done
+            GC.KeepAlive(mutex);
         }
     }
 }
