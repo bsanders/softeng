4416100ecb98f0f222ed7eef0590bc74fcda5e72
Bill Sanders <billysanders@gmail.com>
2013-04-05 13:56:20 -0700

    Fixed incorrect references to thumbnailImage

--- PhotoBombBackend.cs
+++ PhotoBombBackend.cs
@@ -437,7 +437,7 @@ namespace SoftwareEng
 
                 userAlbum.UID = (int)thisAlbum.Attribute("uid");
                 
-                userAlbum.thumbnailPath = thisAlbum.Element("thumbnailImage").Value;
+                userAlbum.thumbnailPath = thisAlbum.Element("thumbnailPath").Value;
 
                 try
                 {
