7d48a3a1a6a6e0165607464f86b2b84404679641
Ryan Causey <rccausey@gmail.com>
2013-04-07 22:32:39 -0700

    Implemented the fixes to make the application only launch a single instance.

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,17 +1,55 @@
-﻿using System;
+﻿/********************************************************************************
+ * Edited this class to implement ISingleInstanceApp to make it so that the
+ * application can only launch one instance.
+ * 
+ * Uses SingleInstance.cs
+ * 
+ * Source: http://blogs.microsoft.co.il/blogs/arik/archive/2010/05/28/wpf-single-instance-application.aspx
+ * -Ryan Causey
+ *******************************************************************************/
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using Microsoft.Shell;
 
 namespace SoftwareEng
 {
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
-    public partial class App : Application
+    public partial class App : Application, ISingleInstanceApp
     {
+        private const string Unique = "Photobomber Unique String";
+
+        [STAThread]
+        public static void Main()
+        {
+            if (SingleInstance<App>.InitializeAsFirstInstance(Unique))
+            {
+                var application = new App();
+
+                application.InitializeComponent();
+                application.Run();
+
+                // Allow single instance code to perform cleanup operations
+                SingleInstance<App>.Cleanup();
+            }
+        }
+
+        #region ISingleInstanceApp Members
+
+        public bool SignalExternalCommandLineArgs(IList<string> args)
+        {
+            // handle command line arguments of second instance
+            // ...
+
+            return true;
+        }
+
+        #endregion
     }
 }
