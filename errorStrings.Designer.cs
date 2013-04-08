e6a4c057338dc689f1d63f7e4969d3a43267ab01
nomoreclowns <sosar001@cougars.csusm.edu>
2013-04-07 22:32:08 -0700

    fixed some error strings

--- errorStrings.Designer.cs
+++ errorStrings.Designer.cs
@@ -8,11 +8,10 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace SoftwareEng
-{
+namespace SoftwareEng {
     using System;
-
-
+    
+    
     /// <summary>
     ///   A strongly-typed resource class, for looking up localized strings, etc.
     /// </summary>
@@ -23,268 +22,229 @@ namespace SoftwareEng
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    internal class errorStrings
-    {
-
+    internal class errorStrings {
+        
         private static global::System.Resources.ResourceManager resourceMan;
-
+        
         private static global::System.Globalization.CultureInfo resourceCulture;
-
+        
         [global::System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
-        internal errorStrings()
-        {
+        internal errorStrings() {
         }
-
+        
         /// <summary>
         ///   Returns the cached ResourceManager instance used by this class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Resources.ResourceManager ResourceManager
-        {
-            get
-            {
-                if (object.ReferenceEquals(resourceMan, null))
-                {
+        internal static global::System.Resources.ResourceManager ResourceManager {
+            get {
+                if (object.ReferenceEquals(resourceMan, null)) {
                     global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("SoftwareEng.errorStrings", typeof(errorStrings).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
             }
         }
-
+        
         /// <summary>
         ///   Overrides the current thread's CurrentUICulture property for all
         ///   resource lookups using this strongly typed resource class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Globalization.CultureInfo Culture
-        {
-            get
-            {
+        internal static global::System.Globalization.CultureInfo Culture {
+            get {
                 return resourceCulture;
             }
-            set
-            {
+            set {
                 resourceCulture = value;
             }
         }
-
+        
         /// <summary>
         ///   Looks up a localized string similar to PhotoBomber failed to create the album, will now try to recover the library..
         /// </summary>
-        internal static string addAlbumFailure
-        {
-            get
-            {
+        internal static string addAlbumFailure {
+            get {
                 return ResourceManager.GetString("addAlbumFailure", resourceCulture);
             }
         }
-
+        
         /// <summary>
-        ///   Looks up a localized string similar to PhotoBomber failed to add the image(s)..
+        ///   Looks up a localized string similar to PhotoBomber failed to import the Image(s)..
         /// </summary>
-        internal static string addImageFailure
-        {
-            get
-            {
+        internal static string addImageFailure {
+            get {
                 return ResourceManager.GetString("addImageFailure", resourceCulture);
             }
         }
-
+        
         /// <summary>
         ///   Looks up a localized string similar to Warning! Unable to add all images..
         /// </summary>
-        internal static string addImageWarning
-        {
-            get
-            {
+        internal static string addImageWarning {
+            get {
                 return ResourceManager.GetString("addImageWarning", resourceCulture);
             }
         }
-
+        
         /// <summary>
         ///   Looks up a localized string similar to Failed to copy items to clipboard..
         /// </summary>
-        internal static string copyToClipboardFailure
-        {
-            get
-            {
+        internal static string copyToClipboardFailure {
+            get {
                 return ResourceManager.GetString("copyToClipboardFailure", resourceCulture);
             }
         }
-
+        
         /// <summary>
         ///   Looks up a localized string similar to Warning! Some items could not be copied to clipboard..
         /// </summary>
-        internal static string copyToClipboardWarning
-        {
-            get
-            {
+        internal static string copyToClipboardWarning {
+            get {
                 return ResourceManager.GetString("copyToClipboardWarning", resourceCulture);
             }
         }
-
+        
         /// <summary>
         ///   Looks up a localized string similar to Failed to delete album, will now try to recover the library..
         /// </summary>
-        internal static string deleteAlbumFailure
-        {
-            get
-            {
+        internal static string deleteAlbumFailure {
+            get {
                 return ResourceManager.GetString("deleteAlbumFailure", resourceCulture);
             }
         }
-
+        
         /// <summary>
         ///   Looks up a localized string similar to Failed to delete the photo..
         /// </summary>
-        internal static string deletePhotoFailure
-        {
-            get
-            {
+        internal static string deletePhotoFailure {
+            get {
                 return ResourceManager.GetString("deletePhotoFailure", resourceCulture);
             }
         }
-
+        
+        /// <summary>
+        ///   Looks up a localized string similar to PhotoBomber failed to import the Image(s)..
+        /// </summary>
+        internal static string failedToPasteImages {
+            get {
+                return ResourceManager.GetString("failedToPasteImages", resourceCulture);
+            }
+        }
+        
         /// <summary>
         ///   Looks up a localized string similar to Failed to retrieve pictures in the album..
         /// </summary>
-        internal static string getPhotosFailure
-        {
-            get
-            {
+        internal static string getPhotosFailure {
+            get {
                 return ResourceManager.GetString("getPhotosFailure", resourceCulture);
             }
         }
-
+        
         /// <summary>
         ///   Looks up a localized string similar to Warning! Some pictures could not be retrieved..
         /// </summary>
-        internal static string getPhotosWarning
-        {
-            get
-            {
+        internal static string getPhotosWarning {
+            get {
                 return ResourceManager.GetString("getPhotosWarning", resourceCulture);
             }
         }
-
+        
         /// <summary>
         ///   Looks up a localized string similar to Album names can only contain letters, numbers and spaces..
         /// </summary>
-        internal static string invalidAlbumNameCharacter
-        {
-            get
-            {
+        internal static string invalidAlbumNameCharacter {
+            get {
                 return ResourceManager.GetString("invalidAlbumNameCharacter", resourceCulture);
             }
         }
-
+        
         /// <summary>
         ///   Looks up a localized string similar to An album with this name already exists..
         /// </summary>
-        internal static string invalidAlbumNameUnique
-        {
-            get
-            {
+        internal static string invalidAlbumNameUnique {
+            get {
                 return ResourceManager.GetString("invalidAlbumNameUnique", resourceCulture);
             }
         }
-
+        
         /// <summary>
         ///   Looks up a localized string similar to Image names can only contain letters, numbers and spaces..
         /// </summary>
-        internal static string invalidImageNameCharacter
-        {
-            get
-            {
+        internal static string invalidImageNameCharacter {
+            get {
                 return ResourceManager.GetString("invalidImageNameCharacter", resourceCulture);
             }
         }
-
+        
         /// <summary>
         ///   Looks up a localized string similar to An image in this album already has this name..
         /// </summary>
-        internal static string invalidImageNameUnique
-        {
-            get
-            {
+        internal static string invalidImageNameUnique {
+            get {
                 return ResourceManager.GetString("invalidImageNameUnique", resourceCulture);
             }
         }
-
+        
         /// <summary>
         ///   Looks up a localized string similar to Names can only contain letters, numbers, spaces, and must have at least one letter or number..
         /// </summary>
-        internal static string invalidNameCharacter
-        {
-            get
-            {
+        internal static string invalidNameCharacter {
+            get {
                 return ResourceManager.GetString("invalidNameCharacter", resourceCulture);
             }
         }
-
+        
         /// <summary>
         ///   Looks up a localized string similar to Failed to rebuild and/or recover the database. We&apos;re sorry :&apos;(.
         /// </summary>
-        internal static string rebuildBackendFailure
-        {
-            get
-            {
+        internal static string rebuildBackendFailure {
+            get {
                 return ResourceManager.GetString("rebuildBackendFailure", resourceCulture);
             }
         }
-
+        
         /// <summary>
         ///   Looks up a localized string similar to Failed to rename the album..
         /// </summary>
-        internal static string renameAlbumFailure
-        {
-            get
-            {
+        internal static string renameAlbumFailure {
+            get {
                 return ResourceManager.GetString("renameAlbumFailure", resourceCulture);
             }
         }
-
+        
         /// <summary>
         ///   Looks up a localized string similar to Failed to rename the picture..
         /// </summary>
-        internal static string renamePhotoFailure
-        {
-            get
-            {
+        internal static string renamePhotoFailure {
+            get {
                 return ResourceManager.GetString("renamePhotoFailure", resourceCulture);
             }
         }
-
+        
         /// <summary>
         ///   Looks up a localized string similar to Failed to retrieve any albums..
         /// </summary>
-        internal static string retrieveAlbumsFailure
-        {
-            get
-            {
+        internal static string retrieveAlbumsFailure {
+            get {
                 return ResourceManager.GetString("retrieveAlbumsFailure", resourceCulture);
             }
         }
-
+        
         /// <summary>
         ///   Looks up a localized string similar to Warning! Some albums could not be retrieved..
         /// </summary>
-        internal static string retrieveAlbumsWarning
-        {
-            get
-            {
+        internal static string retrieveAlbumsWarning {
+            get {
                 return ResourceManager.GetString("retrieveAlbumsWarning", resourceCulture);
             }
         }
-
+        
         /// <summary>
         ///   Looks up a localized string similar to Failed to stop picture import. Just let it try to finish to ensure the library doesn&apos;t become corrupted!.
         /// </summary>
-        internal static string stopImportFailure
-        {
-            get
-            {
+        internal static string stopImportFailure {
+            get {
                 return ResourceManager.GetString("stopImportFailure", resourceCulture);
             }
         }
