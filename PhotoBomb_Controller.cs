fb7352bbf1676f423682eedf53ab5d805adc66f2
myon232 <myon232@gmail.com>
2013-04-27 20:40:10 -0700

    The PhotoBomb Controller class was made but it's not doing anything.

---
+++ PhotoBomb_Controller.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.ComponentModel;
+
+namespace SoftwareEng
+{
+    public class PhotoBomb_Controller
+    {
+
+
+    } // End of PhotoBomb_Controller
+}
