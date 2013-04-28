ded5f35193d53d64aac2c860908ddff257d32877
myon232 <myon232@gmail.com>
2013-04-27 20:25:04 -0700

    The data classes for the xml handler classes were updated

---
+++ AlbumImageXmlData.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SoftwareEng
+{
+    class AlbumImageXmlData
+    {
+        private readonly String _fileHash;
+
+        public String FileHash
+        {
+            get { return _fileHash; }
+        } 
+            
+        private readonly String _imageName;
+
+        public String ImageName
+        {
+            get { return _imageName; }
+        }
+
+        private readonly String _caption;
+
+        public String Caption
+        {
+            get { return _caption; }
+        }
+
+        public AlbumImageXmlData(String fileHash, String imageName, String caption)
+        {
+            _fileHash = fileHash;
+            _imageName = imageName;
+            _caption = caption;
+        }
+
+    } // End of AlbumImageXmlData. 
+}
---
+++ AlbumXmlData.cs
@@ -0,0 +1,56 @@
+﻿/*
+ * Change Log: 
+ * Julian Nguyen (4/26/13)
+ * The class was made.
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SoftwareEng
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    class AlbumXmlData 
+    {
+        private readonly String _albumUID;
+
+        public String AlbumUID
+        {
+            get { return _albumUID; }
+        }
+
+        private readonly String _albumName;
+
+        public String AlbumName
+        {
+            get { return _albumName; }
+        } 
+
+        private readonly String _thumbFileHash;
+
+        public String ThumbFileHash
+        {
+            get { return _thumbFileHash; }
+        } 
+
+        private readonly List<AlbumImageXmlData> _images;
+
+        internal List<AlbumImageXmlData> Images
+        {
+            get { return _images; }
+        } 
+
+
+        
+        public AlbumXmlData(String albumUID, String albumName, String thumbFileHash, List<AlbumImageXmlData> images)
+        {
+            _albumUID = albumUID;
+            _albumName = albumName;
+            _thumbFileHash = thumbFileHash;
+            _images = images;
+        }
+    }
+}
