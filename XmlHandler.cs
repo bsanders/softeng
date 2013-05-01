0c42de4c9c51d80475878855f72bec2fe39f0015
myon232 <myon232@gmail.com>
2013-04-30 20:35:23 -0700

    The XmlHandler class was added to in hopes of removing the xml handling from PhotoBomb class.

---
+++ XmlHandler.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+using System.IO;
+
+namespace SoftwareEng
+{
+    class XmlHandler
+    {
+        // TODO: What are you doing, change this. 
+        private Properties.Settings Settings = Properties.Settings.Default;
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="pathToXml"></param>
+        /// <param name="xmlRootElement"></param>
+        /// <returns></returns>
+        public bool loadXmlRootElement(String pathToXml, out XElement xmlRootElement)
+        {
+
+            try
+            {
+                xmlRootElement = XDocument.Load(pathToXml).Element(Settings.XMLRootElement);
+                return true;
+            }
+            catch (IOException e)
+            {
+                xmlRootElement = null;
+                return false;
+            }
+
+        }
+
+
+    }
+}
