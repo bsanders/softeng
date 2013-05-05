56b688fcd82a6e3c8d1174559f8cc29a305f91a2
nomoreclowns <sosar001@cougars.csusm.edu>
2013-05-04 17:26:43 -0700

    .

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -2319,50 +2319,6 @@ namespace SoftwareEng
 
     }
 
-    /*
-     * Created By: Ryan Causey
-     * Created Date: 4/6/13
-     * Last Edited By: Ryan Causey
-     * Last Edited Date: 4/7/13
-     */
-    /// <summary>
-    /// Converter to allow data binding to be used in the BitmapImage UriSource attribute with the
-    /// cache option on.
-    /// </summary>
-    public class ImagePathConverter : IValueConverter
-    {
-        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
-        {
-            // value contains the full path to the image
-            String path = (String)value;
-            //handled the path not being loaded yet.
-            if (path != "")
-            {
-                if (File.Exists(path))
-                {
-                    // load the image, convert to bitmap, set cache option so it
-                    //does not lock out the file, then return the new image.
-                    BitmapImage image = new BitmapImage();
-                    image.BeginInit();
-                    image.CacheOption = BitmapCacheOption.OnLoad;
-                    image.UriSource = new Uri(path);
-                    image.EndInit();
-
-                    return image;
-                }
-            }
-
-            return DependencyProperty.UnsetValue;
-        }
-
-        //put this here so that if someone tries to convert back we throw an exception as
-        //the operation is not implemented.
-        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
-        {
-            throw new NotImplementedException("The method or operation is not implemented.");
-        }
-    }
-
 }
 
 
